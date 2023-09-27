ec0743b425a4cef2061cecf9ba1751b800bec46a
ages24 <ajegbemiga24@gmail.com>
2023-09-27 09:12:43 +0100

    Implement user name and image display in navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,10 @@ import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from '@/lib/session';
 
-const Navbar = () => {
-  const session = {};
+const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -18,9 +19,17 @@ const Navbar = () => {
         ))}
         </ul>
         <div className="flexCenter gap-4">
-          {session ? (
+          {session?.user ? (
             <>
-            UserPhoto
+            {session?.user?.image && (
+              <Image
+              src={session.user.image}
+              width={40}
+              height={40}
+              alt={session.user.name}
+              className='rounded-full'
+              />
+            )}
             <Link href="/create-project">
               Share Work
             </Link>
